d89fa2f817892cc81f99893ca90707827cc61f77
srikxr <srikarchava5@gmail.com>
2023-02-17 21:49:27 -0800

    fix 1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,9 @@
 import '@/styles/globals.css'
 import localFont from '@next/font/local'
 
-const Sequel = localFont({
-  src: './fonts/SequelBlack.otf',
-  variable: '--font-Sequel-black',
-})
-
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${Sequel.variable} font-sans`}>
+    
       <Component {...pageProps} />
-    </main>
   )
 }

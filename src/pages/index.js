a4a840c1859df955ab6a4dda478d0d93d47f2a02
srikxr <srikarchava5@gmail.com>
2023-02-18 00:01:26 -0800

    whatever

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import axios from "axios";
 
-
-
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -48,27 +45,43 @@ export default function Home() {
     }
   };
 
+const descriptions = [
+  {
+    Title: "Revolutionize Your Aftercare Process",
+    Body: "A one of a kind, automated tool to enhance your aftercare progression."
+
+  },
+  {
+    Title: "Seamless Communication",
+    Body: "Our platform allows you to easily communicate with your healthcare provider via text message."
+
+  },
+  {
+    Title: "A Flexible Medical Solution",
+    Body: "Receive the best possible care and prioritize your health on your own terms."
+
+  },
+]
+
   return (
-    <div className=' w-screen h-screen'>
+    <div className=' w-screen h-screen '>
        <header classname=" top-0 p-5 flex flex-row max-w-7xl mx-auto z-20 justify-center items-center align-middle">
-       <div className="flex ">
-            <h1 className='font-bold font-inter p-5 text-[20px]'>
+            <h1 className='font-bold font-inter ml-[5%] p-5 text-[20px]'>
                 AfterAware
             </h1>
-        </div>
       </header>
 
-      <div className="w-full h-full align-middle  mt-[40%] flex">
+      <div className="w-full h-full align-middle md:mt-[10%]  mt-[30%] flex">
             
-            <div className="w-[80%] mx-auto align-middle flex flex-col">
-              <h1 className="font-bold text-[#3375F2] xl:text-[100px] lg:text-[90px] md:text-[80px] text-[30px] items-left lg:justify-left md:justify-left justify-left lg:text-left md:text-left text-left flex z-0  content-left ">
+            <div className="w-[80%] md:w-[70%] mx-auto align-middle flex flex-col">
+              <h1 className="font-bold text-[#3375F2] md:text-[60px] text-[40px] items-left  lg:justify-left md:justify-left justify-left  md:text-center text-left flex z-0  content-left md:leading-[65px] leading-[45px]">
                  Streamline medical aftercare with easy text check-ins.
               </h1>
-              <p className=" mt-[10px] lg:justify-left md:justify-left justify-center lg:text-left md:text-left text-left font-Amiri font-regular xl:text-[60px] lg:text-[30px] md:text-[10px] text-[15px]">
+              <p className=" mt-[3%]  md:justify-center justify-center lg:text-center md:text-center text-left font-Amiri font-regular md:items-center  lg:text-[20px] md:text-[10px] text-[15px]">
                 Seamlessly connect with your healthcare provider for a streamlined experience.
               </p>
-            <div className='xl:mt-[5%] lg:mt-[7%] md:mt-[7%] mt-[12%] lg:justify-left md:justify-left justify-center lg:text-left md:text-left text-left flex flex-row'>
-            <form onSubmit={handleSubmit}>
+            <div className='xl:mt-[5%] lg:mt-[5%] md:mt-[5%] mt-[12%] lg:justify-left md:justify-center justify-left lg:text-left md:text-left text-left flex flex-row'>
+            <form onSubmit={handleSubmit} className='flex flex-row'>
                 <PhoneInput
                   placeholder="Enter phone number"
                   value={phone}
@@ -78,14 +91,35 @@ export default function Home() {
                 />
                 <button
                 type="submit"
-                className=' bg-[#3375F2] pt-2 pb-2 pl-3 pr-3 rounded-xl text-white font-bold shadow-2xl hover:cursor-grab hover:bg-black hover:text-white '>
+                className=' bg-[#3375F2] ml-[5%] pt-1 pb-1 pl-3 pr-3 rounded-xl text-white font-bold shadow-2xl hover:cursor-grab hover:bg-black hover:text-white '>
                   Start
                 </button>
               </form>
               </div>
-              <p className="lg:justify-left md:justify-left justify-center lg:text-left md:text-left text-left font-light font-Inter text-[#706D6D] lg:text-[13px] md:text-[13px] text-[9px] lg:mt-[2%] md:mt-[2%] mt-[5%]">
-                Disclaimer: This is only an initial screening and should NOT be interpreted as an official diagnosis. 
+              <p className=" flex lg:justify-left md:justify-center justify-left lg:text-left md:text-left text-left font-light font-Inter text-[#706D6D] lg:text-[13px] md:text-[13px] text-[9px] lg:mt-[2%] md:mt-[2%] mt-[5%]">
+                Enter your phone number to recieve a text from your healthcare provider
               </p>
+
+          <div className='mt-[5%] text-left md:text-center md:items-center md:justify-center '>
+            <img 
+              src="https://personal-website-2022.s3.us-west-2.amazonaws.com/AfterAware+mock.png"
+              className='w-[100%] h-[40%] md:w-[75%] md:h-[50%] drop-shadow-xl invisible md:visible align-middle mx-auto'
+            />
+            <div className=''>
+              <h1 className='text-[25px] font-semibold md:text-[40px] mt-[5%] drop-shadow-xl'>
+              Maximize Your Health Recovery With Our Workflow Solutions.
+              </h1>
+            </div>
+            <div className='flex items-left justify-left flex-col md:flex-row mt-[10%] md:mt-[0%] mb-[20%]'>
+              {descriptions.map((description, i) => (
+                <div key={i} className=" mt-[5%] pl-0 md:ml-2 md:mr-2 bg-gray-200 rounded-xl drop-shadow-xl"> 
+                  <h1 className='font-bold p-4'>{description.Title}</h1>
+                  <p className='font-regular text-[13px] pl-3 pr-3 pb-4'>{description.Body}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
 
             </div>
           </div>
